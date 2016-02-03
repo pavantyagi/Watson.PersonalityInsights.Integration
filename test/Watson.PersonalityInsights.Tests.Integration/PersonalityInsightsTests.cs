ce065d378172d23e73898408a1cd05edcb04de8d
Damien Dennehy <damien@dennehy.me>
2016-02-03 15:00:31 -0700

    added additional tests for new Csv method

--- test/Watson.PersonalityInsights.Tests.Integration/PersonalityInsightsTests.cs
+++ test/Watson.PersonalityInsights.Tests.Integration/PersonalityInsightsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -23,6 +22,49 @@ namespace Watson.PersonalityInsights.Tests.Integration
         }
 
         [Fact]
+        public async Task GetProfileAsync_WithString_IsNotNull()
+        {
+            var content = File.ReadAllText("SampleContent1.txt");
+
+            var service = new PersonalityInsightsService(Settings.Username, Settings.Password);
+            var profile = await service.GetProfileAsync(content).ConfigureAwait(false);
+
+            Assert.NotNull(profile);
+        }
+
+        [Fact]
+        public async Task GetProfileAsync_WithOptions_IsNotNull()
+        {
+            var content = File.ReadAllText("SampleContent1.txt");
+
+            var service = new PersonalityInsightsService(Settings.Username, Settings.Password);
+            var options = new ProfileOptions(content) {IncludeRaw = true};
+            var profile = await service.GetProfileAsync(options).ConfigureAwait(false);
+
+            Assert.NotNull(profile);
+        }
+
+        [Fact]
+        public async Task GetProfileAsync_WithContentItems_IsNotNull()
+        {
+            var content1 = File.ReadAllText("SampleContent1.txt");
+            var content2 = File.ReadAllText("SampleContent2.txt");
+            var service = new PersonalityInsightsService(Settings.Username, Settings.Password);
+
+            var contentItems = new List<ContentItem>
+            {
+                new ContentItem(content1),
+                new ContentItem(content2)
+            };
+
+            var content = new Content(contentItems);
+            var options = new ProfileOptions(content);
+            var profile = await service.GetProfileAsync(options).ConfigureAwait(false);
+
+            Assert.NotNull(profile);
+        }
+
+        [Fact]
         public async Task GetProfileAsCsvAsync_IsNotNull()
         {
             var content = File.ReadAllText("SampleContent1.txt");
@@ -40,19 +82,19 @@ namespace Watson.PersonalityInsights.Tests.Integration
         }
 
         [Fact]
-        public async Task GetProfileAsync_IsNotNull()
+        public async Task GetProfileAsCsvAsync_WithOptions_IsNotNull()
         {
             var content = File.ReadAllText("SampleContent1.txt");
 
             var service = new PersonalityInsightsService(Settings.Username, Settings.Password);
-            var options = new ProfileOptions(content) {AcceptLanguage = AcceptLanguage.Es};
-            var profile = await service.GetProfileAsync(options).ConfigureAwait(false);
+            var options = new ProfileOptions(content) {IncludeRaw = true};
+            var profile = await service.GetProfileAsCsvAsync(options).ConfigureAwait(false);
 
-            Assert.NotNull(profile);
+            Assert.False(string.IsNullOrWhiteSpace(profile));
         }
 
         [Fact]
-        public async Task GetProfileAsync_WithContentItems_IsNotNull()
+        public async Task GetProfileAsCsvAsync_WithContentItems_IsNotNull()
         {
             var content1 = File.ReadAllText("SampleContent1.txt");
             var content2 = File.ReadAllText("SampleContent2.txt");
@@ -60,41 +102,15 @@ namespace Watson.PersonalityInsights.Tests.Integration
 
             var contentItems = new List<ContentItem>
             {
-                new ContentItem
-                {
-                    Content = content1,
-                    Charset = "utf-8",
-                    ContentType = "text/plain",
-                    Created = DateTimeOffset.UtcNow,
-                    Forward = false,
-                    Id = Guid.NewGuid().ToString(),
-                    ContentLanguage = ContentLanguage.En,
-                    Reply = false,
-                    SourceId = "blog",
-                    Updated = DateTimeOffset.UtcNow,
-                    UserId = "RogerEbert"
-                },
-                new ContentItem
-                {
-                    Content = content2,
-                    Charset = "utf-8",
-                    ContentType = "text/plain",
-                    Created = DateTimeOffset.UtcNow,
-                    Forward = false,
-                    Id = Guid.NewGuid().ToString(),
-                    ContentLanguage = ContentLanguage.En,
-                    Reply = false,
-                    SourceId = "blog",
-                    Updated = DateTimeOffset.UtcNow,
-                    UserId = "RogerEbert"
-                }
+                new ContentItem(content1),
+                new ContentItem(content2)
             };
 
             var content = new Content(contentItems);
             var options = new ProfileOptions(content);
-            var profile = await service.GetProfileAsync(options).ConfigureAwait(false);
+            var profile = await service.GetProfileAsCsvAsync(options).ConfigureAwait(false);
 
-            Assert.NotNull(profile);
+            Assert.False(string.IsNullOrWhiteSpace(profile));
         }
     }
 }
